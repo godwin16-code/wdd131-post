825c4fdc623fe3386309dc6583ecf70e72859801
godwin16-code <Iwajomogodwin72@gmail.com>
2026-03-26 14:39:32 +0100

    Portfolio page

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+document.getElementById("course-title").textContent = "WDD 131: Dynamic Web Fundamentals";
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
